d49d34a8181f5c4f6f84acc5a7fd7dcdfae5edce
Aakash <55752138+aakash-cse@users.noreply.github.com>
2020-11-15 07:24:54 +0530

    Create Day20.cpp

---
+++ Day20/Day20.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int a_i = 0; a_i < n; a_i++){
+    	cin >> a[a_i];
+    }
+    // Write Your Code Here
+    int numberOfSwaps = 0;
+    for (int i = 0; i < n; i++) {
+    // Track number of elements swapped during a single array traversal
+    
+    for (int j = 0; j < n - 1; j++) {
+        // Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+            numberOfSwaps++;
+        }
+    }
+
+    }
+    cout<<"Array is sorted in "<<numberOfSwaps<<" swaps."<<endl;
+    cout<<"First Element: "<<a[0]<<endl;
+    cout<<"Last Element: "<<a[n-1]<<endl;
+}
