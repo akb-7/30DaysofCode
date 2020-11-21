4a66f1e49d624e20bef5d4efb87c9e34e9d7b27d
Aakash <55752138+aakash-cse@users.noreply.github.com>
2020-11-21 11:16:21 +0530

    Update Day27.cpp

--- Day27/Day27.cpp
+++ Day27/Day27.cpp
@@ -23,7 +23,8 @@ int minimum_index(vector<int> seq) {
 class TestDataEmptyArray {
 public:
     static vector<int> get_array() {
-        // complete this function
+        vector<int> arr {};
+        return arr;
     }
 
 };
@@ -31,11 +32,12 @@ public:
 class TestDataUniqueValues {
 public:
     static vector<int> get_array() {
-        // complete this function
+        vector<int> arr {1, 2, 3, 4, 5};
+        return arr;
     }
 
     static int get_expected_result() {
-        // complete this function
+        return 0;
     }
 
 };
@@ -43,12 +45,54 @@ public:
 class TestDataExactlyTwoDifferentMinimums {
 public:
     static vector<int> get_array() {
-        // complete this function
+        vector<int> arr {1, 3, 1};
+        return arr;
     }
 
     static int get_expected_result() {
-        // complete this function
+        return 0;
     }
 
 };
 
+void TestWithEmptyArray() {
+    try {
+        auto seq = TestDataEmptyArray::get_array();
+        auto result = minimum_index(seq);
+    } catch (invalid_argument& e) {
+        return;
+    }
+    assert(false);
+}
+
+void TestWithUniqueValues() {
+    auto seq = TestDataUniqueValues::get_array();
+    assert(seq.size() >= 2);
+
+    assert(set<int>(seq.begin(), seq.end()).size() == seq.size());
+
+    auto expected_result = TestDataUniqueValues::get_expected_result();
+    auto result = minimum_index(seq);
+    assert(result == expected_result);
+}
+
+void TestWithExactlyTwoDifferentMinimums() {
+    auto seq = TestDataExactlyTwoDifferentMinimums::get_array();
+    assert(seq.size() >= 2);
+
+    auto tmp = seq;
+    sort(tmp.begin(), tmp.end());
+    assert(tmp[0] == tmp[1] and (tmp.size() == 2 or tmp[1] < tmp[2]));
+
+    auto expected_result = TestDataExactlyTwoDifferentMinimums::get_expected_result();
+    auto result = minimum_index(seq);
+    assert(result == expected_result);
+}
+
+int main() {
+    TestWithEmptyArray();
+    TestWithUniqueValues();
+    TestWithExactlyTwoDifferentMinimums();
+    cout << "OK" << endl;
+    return 0;
+}
