5a3b1a9a17ac6c52229594e5ca1ec073586bd140
Aakash <55752138+aakash-cse@users.noreply.github.com>
2020-11-22 10:22:11 +0530

    Create Day28.py

---
+++ Day28/Day28.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+if __name__ == '__main__':
+    N = int(input())
+    
+    result = []
+
+    for N_itr in range(N):
+        firstNameEmailID = input().split()
+
+        firstName = firstNameEmailID[0]
+
+        emailID = firstNameEmailID[1]
+        
+        if '@gmail.com' in emailID:
+            result.append(firstName)
+        result.sort()
+            
+    for val in result:
+        print(val)
+        
